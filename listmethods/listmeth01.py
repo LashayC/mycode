f30130881420db99bbf1ea044a904a4217f5d864
Lashay Cokley <LashayC@users.noreply.github.com>
2022-09-20 19:46:13 +0000

    manipulating lists

---
+++ listmethods/listmeth01.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+def main():
+
+    proto = ["ssh", "http", "https"]
+    
+    protoa = ["ssh", "http", "https"]
+
+    print(proto)
+
+    proto.append("dns")
+
+    protoa.append("dns")
+
+    print(proto)
+
+    proto2 = [22, 80, 443, 53]
+
+    proto.extend(proto2)
+
+    print(proto)
+
+    protoa.append(proto2)
+    
+    print(protoa)
+
+    protoa.insert(5, 64)
+
+    print(protoa)
+
+if __name__ == "__main__":
+    main()
